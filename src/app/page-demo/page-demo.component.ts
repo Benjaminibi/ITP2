9255562044d3fd5d7eaa10fc6eedeb636598c59a
200326 <200326@studierende.htl-donaustadt.at>
2024-12-04 09:49:20 +0100

    Name wurde in Großbuchstaben angegeben. User Story: 25

--- src/app/page-demo/page-demo.component.ts
+++ src/app/page-demo/page-demo.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { Person, MINNIE } from '../person';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page-demo',
   templateUrl: './page-demo.component.html',
   styleUrls: ['./page-demo.component.scss'],
   standalone: true,
+  imports: [CommonModule], // Importiert CommonModule für Pipes wie uppercase
 })
 export class PageDemoComponent {
   me: Person = MINNIE;
+
+  // Optional: Getter für Blockschrift
+  get uppercaseSurname(): string {
+    return this.me.surname.toUpperCase();
+  }
 }
